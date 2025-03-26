7ac4b0ce7d0463290ce4f072bf610f224ed3cbdf
pablojg25 <pablojorgegorrn@gmail.com>
2025-03-26 14:37:10 +0000

    repositories created

---
+++ src/main/java/com/pablo/calendar_backend/repository/UserNotificationRepository.java
@@ -0,0 +1,24 @@
+package com.pablo.calendar_backend.repository;
+
+import com.pablo.calendar_backend.entity.UserNotification;
+import com.pablo.calendar_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserNotificationRepository extends JpaRepository<UserNotification,Long> {
+
+    Optional<UserNotification> findByIdAndUser(Long id, User user);
+
+    List<UserNotification> findByUser(User user);
+
+    UserNotification save(UserNotification notification);
+
+    void delete(UserNotification notification);
+
+    void deleteByUser(User user);
+
+}
---
+++ src/main/java/com/pablo/calendar_backend/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package com.pablo.calendar_backend.repository;
+
+import com.pablo.calendar_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findById(Long id);
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    User save(User user);
+
+    void delete(User user);
+
+}
