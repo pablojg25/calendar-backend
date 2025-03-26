b8382fc170529913d797bc85e8611310b75fb53d
pablojg25 <pablojorgegorrn@gmail.com>
2025-03-26 10:59:48 +0000

    initial commit

---
+++ src/main/java/com/pablo/calendar_backend/CalendarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.calendar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablo/calendar_backend/CalendarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.calendar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
