bad06bd6d935e23fc42da57549535d46a1c5c0fd
pablojg25 <pablojorgegorrn@gmail.com>
2025-03-27 16:37:45 +0000

    allowed cors on controllers

--- src/main/java/com/pablo/calendar_backend/controller/UserNotificationController.java
+++ src/main/java/com/pablo/calendar_backend/controller/UserNotificationController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/notifications")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserNotificationController {
 
     @Autowired
