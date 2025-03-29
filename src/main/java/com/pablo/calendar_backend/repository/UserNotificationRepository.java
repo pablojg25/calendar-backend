d40a6fbadf3be7e239e28b3caf7669e92ebb616a
pablojg25 <pablojorgegorrn@gmail.com>
2025-03-29 16:01:16 +0000

    implemented deleting past notifications

--- src/main/java/com/pablo/calendar_backend/repository/UserNotificationRepository.java
+++ src/main/java/com/pablo/calendar_backend/repository/UserNotificationRepository.java
@@ -2,8 +2,11 @@ package com.pablo.calendar_backend.repository;
 
 import com.pablo.calendar_backend.entity.UserNotification;
 import com.pablo.calendar_backend.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -23,7 +26,9 @@ public interface UserNotificationRepository extends JpaRepository<UserNotificati
 
     void deleteByUser(User user);
 
+    @Modifying
+    @Transactional
     @Query("DELETE FROM UserNotification u WHERE user = :user AND date < :date")
-    void deletePastUserNotifications(User user, LocalDate date);
+    void deletePastUserNotifications(@Param("user") User user, @Param("date") LocalDate date);
 
 }
